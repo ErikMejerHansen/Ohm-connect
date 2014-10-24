547b416c878ff325e79694f2fdee4154fbbdfa90
Erik Mejer Hansen <emh@silverbullet.dk>
2014-10-24 19:32:00 +0200

    Added code for challenge 01 connector

--- 01_create_team.rb
+++ 01_create_team.rb
@@ -8,10 +8,6 @@ data_bits = 8
 stop_bits = 1
 parity = SerialPort::NONE
 
-if !ENV['ARDUINO_PORT']
-	puts 'ARDUINO_PORT env. variable not set!'
-	exit -1
-end
 
 if !ENV['OHM_SERVER']
 	puts 'OHM_SERVER env. variable not set!'
@@ -26,15 +22,18 @@ end
 puts "Started"
 
 while true do 
-	sleep 5
+	sleep 0.5
 	sp = SerialPort.new(port_str, baud_rate, data_bits, stop_bits, parity)
 
 	line = sp.first
 	if(line) 
+		puts "Got: #{line}. Sending to server. Please wait."
 		uri = URI.parse(ENV['OHM_SERVER'])
+		puts "/api/teams/#{ENV['TEAM_ID']}/name/#{line}"
 		http = Net::HTTP.new(uri.hostname, uri.port)
-		request = Net::HTTP::Put.new("/api/teams/#{ENV['TEAM_ID']}")
+		request = Net::HTTP::Put.new("/api/teams/#{ENV['TEAM_ID']}/name/#{URI::encode line.chop }")
 		http.request(request)
+		puts "Completed."
 		exit
 	end
  	sp.flush_input
